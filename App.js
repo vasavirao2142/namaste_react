275d4ed1854242e7bbfbc6b51e839fdc2146b2df
vasavirao2142 <vasaviraochebrolu2142@gmail.com>
2025-03-12 21:49:53 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading=React.createElement('h1',{id:"heading"},'This is a sample heading'); // it retruns a react element or a javascript object 
+const root=ReactDOM.createRoot(document.getElementById('root'));// it creates a root element
+root.render(heading) // it converts react element into a h1 tag 
