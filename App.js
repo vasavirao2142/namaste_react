c3f3a33dda6db62f7918e90a62ad7837086c5da0
vasavirao2142 <vasaviraochebrolu2142@gmail.com>
2025-03-21 21:56:38 +0530

    Addded basic code

--- App.js
+++ App.js
@@ -1,3 +1,92 @@
-const heading=React.createElement('h1',{id:"heading"},'This is a sample heading'); // it retruns a react element or a javascript object 
-const root=ReactDOM.createRoot(document.getElementById('root'));// it creates a root element
-root.render(heading) // it converts react element into a h1 tag 
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+
+// // const heading=React.createElement('h1',{id:"heading"},'This is a sample heading'); // it retruns a react element or a javascript object 
+// const root=ReactDOM.createRoot(document.getElementById('root'));// it creates a root element
+
+
+// // Jsx in another Way
+
+// // Jsx is transpiled into React.createElement and then into Js For browser to Understand
+// //Transpilation is done By babel
+
+// // Jsx => React.createElement=> React Element  =>Js Object =>Html Element
+// const Jsxheading=(
+// <h1>Hi this is a Jsx</h1>)
+// // root.render(heading) // it converts react element into a h1 tag
+// root.render(Jsxheading) // Here we are rendering a react element
+
+// // Functional Element Example
+// // Const heading=(
+// {/* <h1>This is example of react Element </h1> */}
+//     //)
+
+//    // ()=>means return
+
+
+// const HeadingComponent=()=>{
+//     return(
+//         <div>Hi This is A functional Component</div>
+//     )
+// }
+// root.render(<HeadingComponent/>) // Here we are Rendering a Functional component not an element
+
+const RestarauntCard = () => {
+    return (
+        <div className="card">
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2025/2/16/1c06f98e-3bf2-49d0-beda-5cd9e5a60155_7687f083-ea02-40dd-96c7-ea409a4037af.jpg" alt="res-img" className="res-logo" />
+            <h3>Bawarchi</h3>
+            <h4>5 stars</h4>
+            <h4>$400 for two</h4>
+            <h4>20-25 min</h4>
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body-container">
+            <div className="search-conatiner">
+                <p>Search</p>
+            </div>
+            <div className="res-container">
+                <RestarauntCard />
+               
+            </div>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div className="header-container">
+
+            <img src='https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-logo-by-abhishek-choudhary-dribbble.jpg' alt="logo" className="logo-img" />
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout />)
---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detects the React version
+      },
+    },
+  },
+];
